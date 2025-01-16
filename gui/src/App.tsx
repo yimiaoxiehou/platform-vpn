8b3500c9209b67e87d0c8508a62aadccf801049a
yimiao <yimiaoxiehou@gmail.com>
2025-01-16 17:07:30 +0800

    update version to 1.0.0

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -87,7 +87,7 @@ const App: React.FC = () => {
             }}
           />
         </div>
-       <span style={{color: '#dfe4ea', fontWeight: 'bold'}}>version: 1.0.0<br/>2025-01-16 17:02:45</span>
+       <span style={{color: '#dfe4ea', fontWeight: 'bold'}}>version: 1.0.0<br/>2025-01-16 17:07:30</span>
       </Sider>
       <Layout style={{ marginLeft: 200 }} >
         <Content
