df0ca0235f257d16ac20888e834b3dd95abe9594
yimiao <yimiaoxiehou@gmail.com>
2025-01-16 15:34:44 +0800

    update version to v1.2

--- update_version.sh
+++ update_version.sh
@@ -6,9 +6,25 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# 替换 wails.json 中的 productVersion
-sed -i "s/\"productVersion\": \".*\"/\"productVersion\": \"$1\"/" wails.json
-sed -i "s/\"version\": \".*\"/\"version\": \"$1\"/" gui/package.json
+# 方法: 判断系统类型
+OS_TYPE=$(uname)
+if [[ "$OS_TYPE" == "Darwin" ]]; then
+    # macOS
+    # 替换 wails.json 中的 productVersion
+    sed -i "" "s|\"productVersion\": \".*\"|\"productVersion\": \"$1\"|" wails.json
+    sed -i "" "s|\"version\": \".*\"|\"version\": \"$1\"|" gui/package.json
+    sed -i "" "s|Platform VPN .*</h2>|Platform VPN $1</h2>|" gui/src/App.tsx
+elif [[ "$OS_TYPE" == "Linux" ]]; then
+    # Linux
+    # 替换 wails.json 中的 productVersion
+    sed -i "s|\"productVersion\": \".*\"|\"productVersion\": \"$1\"|" wails.json
+    sed -i "s|\"version\": \".*\"|\"version\": \"$1\"|" gui/package.json
+    sed -i "s|Platform VPN .*</h2>|Platform VPN $1</h2>|" gui/src/App.tsx
+else
+    echo "不支持的操作系统类型: $OS_TYPE"
+    exit 1
+fi
+
 echo "版本号已更新为 $1" 
 
 git add .
